96005fda421b311af1b82e713be8498524745cd7
Shibaan Askari <askarishibaan1@gmail.com>
2026-06-04 16:57:50 +0530

    feat: display products from api on page

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const cartbtn = document.getElementById('cart-btn')
 const cartmodal = document.getElementById('cart-modal')
 const closebtn = document.getElementById('close')
+const productcontainer = document.getElementById('products-container')
 let allproducts = null
+
 
 cartbtn.addEventListener('click', () => {
 
@@ -22,12 +24,39 @@ const fetchproducts = async () => {
         const response = await fetch(url)
         const data = await response.json()
         allproducts = data
-
+        displayproducts()
 
     } catch (error) {
         console.log(error)
-
     }
+}
+
+const displayproducts = () => {
+    allproducts.forEach((item) => {
+
+        const newdiv = document.createElement('div')
+        newdiv.classList.add('product-card')
+
+        const img = document.createElement('img')
+        const title = document.createElement('h3')
+        const price = document.createElement('p')
+        const btn = document.createElement('button')
+
+        img.src = item.image
+        title.textContent = item.title
+        price.textContent = `$${item.price}`
+        btn.textContent = 'add to cart'
+        btn.classList.add('add-to-cart')
+
+        newdiv.appendChild(img)
+        newdiv.appendChild(title)
+        newdiv.appendChild(price)
+        newdiv.appendChild(btn)
+
+        productcontainer.appendChild(newdiv)
+
+    });
+
 }
 
 fetchproducts()
